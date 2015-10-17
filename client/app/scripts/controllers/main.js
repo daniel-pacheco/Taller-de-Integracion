ea98f7b91413af3658ff9ce003cd87472f8640d1
daniel-pacheco <daniel_ese_pacheco@hotmail.com>
2015-10-16 23:54:24 -0300

    Directivo nuevo perfil agregado with sidebar and container fluid full width

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,21 +8,35 @@
  * Controller of the clientAppApp
  */
 angular.module('clientAppApp')
-  .controller('MainCtrl', function ($scope, $aside) {
+  .controller('MainCtrl', function ($scope, $aside, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     
+    $scope.data = {};
+
+    $http.get('http://192.168.0.101:8080/EscuelaSantaLucia/rest/sAlumno/listAll').success(function(response){
+
+      $scope.data = response;
+
+    });
+
+
     $scope.tooltip = {
-      "title": "Hello Tooltip<br />This is a multiline message!",
+      "title": "Hello Tooltip <br/>This is a multiline message!",
       "checked": false
     }
 
     $scope.aside = {
   "title": "Title huuu",
-  "content": "Hello Aside<br />This is a multiline message!"
+  "content": "Hello Aside <br/>This is a multiline message!"
+  };
+
+  $scope.modal = {
+    "title": "Title",
+    "content": "ventanita modal!"
   };
 
     $scope.HolaMundo ='Hola mundo';
