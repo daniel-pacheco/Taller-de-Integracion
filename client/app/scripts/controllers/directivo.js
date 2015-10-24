f9cf68c396c2b22552e1bdab38f2d52fa51726b2
daniel-pacheco <daniel_ese_pacheco@hotmail.com>
2015-10-23 21:44:02 -0300

    minor changes

--- client/app/scripts/controllers/directivo.js
+++ client/app/scripts/controllers/directivo.js
@@ -8,10 +8,21 @@
  * Controller of the clientAppApp
  */
 angular.module('clientAppApp')
-  .controller('DirectivoCtrl', function () {
+  .controller('DirectivoCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+$scope.listado1 = true;
+
+$scope.seleccionar = function (id){
+
+	if (id === 'listado') {
+		$scope.listado1 = true;
+	}else if (id === 'nuevoPerfil'){
+		$scope.listado1 = false;
+	};
+}
+
   });
