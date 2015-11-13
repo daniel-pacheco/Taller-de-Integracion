7655d61b679da3bc00a90d8db10bb47c105ddec7
daniel-pacheco <daniel_ese_pacheco@hotmail.com>
2015-11-13 18:12:45 -0300

    alumnoService & a call to it working

--- client/app/scripts/controllers/directivo.js
+++ client/app/scripts/controllers/directivo.js
@@ -50,18 +50,20 @@ var getAll = function(){
       });
       return deferred.promise;
   };
-getAll();
+getAll();*/
 
 $scope.answer = {};
-$scope.answer = alumnoService.alumnoGetAll().succes(function(response));
-*/
+alumnoService.alumnoGetAll().then(function(response){
+  $scope.answer = response.data;  
+});
 
+/*
 $scope.data = [];
 
-    $http.get('http://192.168.0.110:8080/EscuelaSantaLucia/rest/sAlumno/listAll').success(function(response){
+    $http.get('http://192.168.1.11:8080/EscuelaSantaLucia/rest/sAlumno/listAll').success(function(response){
 
       $scope.data = response;
 
-    });
+    });*/
 
 });
