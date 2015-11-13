44305f55b5e73776b3d6d10843af3f4121916cb7
daniel-pacheco <daniel_ese_pacheco@hotmail.com>
2015-11-13 20:21:42 -0300

    ng-repeat filtrando objetos con errorcitos

--- client/app/scripts/controllers/directivo.js
+++ client/app/scripts/controllers/directivo.js
@@ -34,36 +34,19 @@ $scope.search = function () {
     $scope.listFilterIsEnabled = true;
   };
 }
-/*  
-var getAll = function(){
-    var deferred = $q.defer();
-    var url = 'http://192.168.0.110:8080/EscuelaSantaLucia/rest/sAlumno/listAll';
-    $http.get(url)
-      .success(function(response){
-        $scope.answer = response.data;
-                
-        deferred.resolve(response);
-      })
-      .error(function(response){
-        alert("error");
-        deferred.reject("Error al intentar traer datos del usuario");
-      });
-      return deferred.promise;
-  };
-getAll();*/
 
+//---Llamadas al servicio ALUMNO---
 $scope.answer = {};
 alumnoService.alumnoGetAll().then(function(response){
   $scope.answer = response.data;  
 });
 
-/*
-$scope.data = [];
-
-    $http.get('http://192.168.1.11:8080/EscuelaSantaLucia/rest/sAlumno/listAll').success(function(response){
-
-      $scope.data = response;
-
-    });*/
+//---test
+$scope.friends = [{name:'John', phone:'555-1276'},
+                         {name:'Mary', phone:'800-BIG-MARY'},
+                         {name:'Mike', phone:'555-4321'},
+                         {name:'Adam', phone:'555-5678'},
+                         {name:'Julie', phone:'555-8765'},
+                         {name:'Juliette', phone:'555-5678'}];
 
 });
--- client/app/scripts/services/alumnoservice.js
+++ client/app/scripts/services/alumnoservice.js
@@ -11,7 +11,7 @@ angular.module('clientAppApp')
   .service('alumnoService', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var server = 'http://192.168.1.11:8080/'; //http://192.168.0.101:8080/EscuelaSantaLucia/rest/sAlumno/listAll
+    var server = 'http://192.168.1.6:8080/'; //http://192.168.0.101:8080/EscuelaSantaLucia/rest/sAlumno/listAll
     var query = 'EscuelaSantaLucia/rest/sAlumno/listAll';
 
     this.alumnoGetAll = function () {
