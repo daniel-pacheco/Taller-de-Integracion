227703004b5259e3203590008936f401d2a2778c
MartinHerrlein <herrlein.martin@gmail.com>
2015-12-01 16:51:51 -0300

    empece con materias .html y .js

--- client/app/scripts/controllers/materias.js
+++ client/app/scripts/controllers/materias.js
@@ -8,10 +8,21 @@
  * Controller of the clientAppApp
  */
 angular.module('clientAppApp')
-  .controller('MateriasCtrl', function () {
+  .controller('MateriasCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+$scope.listado1 = true;
+
+$scope.seleccionar() = function(id) {
+		if (id === 'listado') {
+		$scope.listado1 = true;
+	}else if (id === 'nuevaMateria'){
+		$scope.listado1 = false;
+	};
+};
   });
+
