94fc7f80f1dbb9c562ed4757d37b4c9eed27ed9b
Eric Pennachini <eric.pennachini@gmail.com>
2016-06-18 01:12:09 -0300

    Primera versión de recuperación de contraseña vía mail. Trabaja con DNI y rol de usuario.

--- server/EscuelaSantaLucia/src/ar/com/santalucia/PruebaMail.java
+++ server/EscuelaSantaLucia/src/ar/com/santalucia/PruebaMail.java
@@ -9,18 +9,25 @@ import javax.mail.internet.MimeMessage;
 public class PruebaMail {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// para probar , ir a https://myaccount.google.com/security
+		// cambiar el punto que dice "Permitir el acceso de aplicaciones menos seguras: SI"
 		System.out.println(">> Setting username and password...");
-		final String userName = "eric.pennachini@gmail.com";
-		final String password = "metalcristiano";
+//		final String userName = "eric.pennachini@gmail.com";
+//		final String password = "metalcristiano";
+		final String userName = "ericdp_0591@hotmail.com";
+		final String password = "mglatu";
 		System.out.println("Done!");
 
 		System.out.println(">> Setting properties...");
 		Properties props = new Properties();
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.host", "smtp.gmail.com");		
-		props.put("mail.smtp.port", "465");
+//		props.put("mail.smtp.socketFactory.port", "465");
+//		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//		props.put("mail.smtp.host", "smtp.gmail.com");		
+//		props.put("mail.smtp.port", "465");
+//		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp-mail.outlook.com");		
+		props.put("mail.smtp.port", "587"); // o 25
 		props.put("mail.smtp.auth", "true");
 		System.out.println("Done!");
 		
@@ -35,9 +42,9 @@ public class PruebaMail {
 		try {
 			System.out.println(">> Building the message...");
 			Message message = new MimeMessage(session);
-			String from = "eric.pennachini@gmail.com";
-			// String to = "ericdp_0591@hotmail.com";
-			String to = "mauricioarielramirez@gmail.com";
+			String from = "ericdp_0591@hotmail.com";
+			String to = "eric.pennachini@gmail.com";
+			// String to = "mauricioarielramirez@gmail.com";
 			message.setFrom(new InternetAddress(from));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
 			message.setSubject("Test subject");
